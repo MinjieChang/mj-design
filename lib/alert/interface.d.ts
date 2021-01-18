ab5a2dd4d628416fd62aaefb54a1a7d52bb10518
changminjie <changminjie@meituan.com>
2021-01-18 17:41:29 +0800

    chapter-3: 导出声明文件

---
+++ lib/alert/interface.d.ts
@@ -0,0 +1,5 @@
+export declare type Kind = 'info' | 'positive' | 'negative' | 'warning';
+export declare type KindMap = Record<Kind, string>;
+export declare type AlertProps = {
+    kind?: 'info' | 'positive' | 'negative' | 'warning';
+};
