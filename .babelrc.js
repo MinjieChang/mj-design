01361c093b6ddc53bf4a90a12f12a46ba3f98563
changminjie <changminjie@meituan.com>
2021-01-18 21:08:05 +0800

    chapter-4: 打包js文件

---
+++ .babelrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: ['@babel/env', '@babel/typescript', '@babel/react'],
+  plugins: ['@babel/plugin-transform-runtime', '@babel/proposal-class-properties'],
+  env: {
+    esm: {
+      presets: [
+        [
+          '@babel/env',
+          {
+            modules: false, // 关闭模块转换
+          },
+        ],
+      ],
+      plugins: [
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            useESModules: true, // 使用esm形式的helper
+          },
+        ],
+      ],
+    },
+  },
+};
