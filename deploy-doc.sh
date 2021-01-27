accdd6afc2838e9c8df9af0dacfe33c591e08705
changminjie <changminjie@meituan.com>
2021-01-27 16:42:54 +0800

    deploy

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -16,7 +16,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-npm run publish:doc
+npm run push:doc
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
