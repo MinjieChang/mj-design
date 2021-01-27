7bd514c9ca61cc309fa9595f00a44dce5c5eacff
changminjie <changminjie@meituan.com>
2021-01-27 16:30:01 +0800

    修改文档部署根路径

--- doczrc.js
+++ doczrc.js
@@ -2,5 +2,6 @@ export default {
   files: './components/**/*.{md,markdown,mdx}', // 识别的文件后缀
   dest: 'doc-site', // 打包出来的文件目录名
   title: 'mj-design', // 站点标题
+  base: '/mj-design',
   typescript: true, // 组件源文件是通过typescript开发，需要打开此选项
 };
